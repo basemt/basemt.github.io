8972461a77c62b7788da3c05ce6625c363f20969
basemt <112492944+basemt@users.noreply.github.com>
2023-04-04 02:16:18 -0400

    elastic collection

---
+++ entries/projects/project 2/elastic2.js
@@ -0,0 +1,103 @@
+  // define our variables
+let toggleMenu = document.querySelector(".mobile-menu");
+let headerStatus = document.querySelector(".header");
+
+// when user clicks on "menu", open and close the mobile navigation
+toggleMenu.addEventListener( "click", () => {
+    if (headerStatus.classList.contains("open")) {
+      headerStatus.classList.remove("open");
+    } else {
+      headerStatus.classList.add("open");
+    }
+  },
+  false
+);
+  
+  // Load the JSON file named elastic2.json
+  fetch('elastic2.json')
+  .then(response => response.json())
+  .then(data => {
+
+    // Access the div element
+    const celebs = document.getElementById('celebs');
+    const btnBouba = document.getElementById('btnBouba');
+    const btnKiki = document.getElementById('btnKiki');
+    const btnMusic = document.getElementById('btnMusic');
+    const btnTv = document.getElementById('btnTv');
+    const btnSports = document.getElementById('btnSports');
+    const btnPolitics = document.getElementById('btnPolitics');
+    const btnMisc = document.getElementById('btnMisc');
+
+    // Function to filter data based on verdict
+    function filterData(filter, verdict, sort = 'desc') {
+      // Clear the current contents of the div
+      celebs.innerHTML = '';
+      let filteredData;
+      //default 
+      filteredData = data;
+        shuffle(filteredData);
+
+      function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+
+      // Filter the data based on the verdict
+      if(filter==="verdict"){
+          filteredData = data.filter(item => item.Verdict.includes(verdict))
+        .sort((a, b) => b.Percentage - a.Percentage);
+      }else if(filter==="category"){
+        filteredData = data.filter(item => item.Category.includes(verdict))
+        .sort((a, b) => b.Percentage - a.Percentage);
+      }
+      
+
+      // Loop through the filtered data and add each item to the div
+      filteredData.forEach(item => {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        const img = document.createElement('img');
+
+        // Set the text content and href attributes for the link
+        p.textContent = item.Name;
+        p2.textContent = item.Verdict;
+        p3.textContent = item.Percentage + "%";
+        img.src = "img/faces/" + item.Img;
+
+        // Add the HTML tags to webpage
+        div.appendChild(p);
+        div.appendChild(img);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        celebs.appendChild(div);
+      });
+    }
+
+    // Add event listeners to the buttons
+    btnBouba.addEventListener('click', () => filterData("verdict", 'Bouba', 'asc'));
+    btnKiki.addEventListener('click', () => filterData("verdict", 'Kiki', 'asc'));
+    btnMusic.addEventListener('click', () => filterData("category", 'Music', 'asc'));
+    btnTv.addEventListener('click', () => filterData("category",'Tv/Film', 'asc'));
+    btnSports.addEventListener('click', () => filterData("category",'Sports', 'asc'));
+    btnPolitics.addEventListener('click', () => filterData("category",'Politics', 'asc'));
+    btnMisc.addEventListener('click', () => filterData("category",'Misc.', 'asc'));
+    
+    // Display all data initially
+    filterData('');
+  })
+  .catch(error => console.error(error));
